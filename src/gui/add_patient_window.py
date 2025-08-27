ff5799923e4f7ae7b48bf1869649cb5310c9b309
nadavreu <nadavreubens8@gmail.com>
2025-08-27 05:25:28 +0300

    nadav first test

--- src/gui/add_patient_window.py
+++ src/gui/add_patient_window.py
@@ -145,7 +145,6 @@ class AddPatientWindow(BaseWindow):
         self.home_window.show()
         self.close()
 
-
     def closeEvent(self, event):
         """Handle window close event."""
         event.accept() 
